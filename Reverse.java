85f040be026c2b21c88743dce2242c2bcd387d34
d0rarara <dorcaskumbu@outlook.com>
2024-11-08 17:53:14 -0500

    implememt Reverse class

--- Reverse.java
+++ Reverse.java
@@ -1,3 +1,16 @@
+// Write a program that reads a string and prints the reverse of the string.
+import java.util.Scanner; 
 public class Reverse {
-    
+    public static void main(String[] arga){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string: ");
+        String input = sc.nextLine();
+        String reverse = "";
+        for(int i = input.length() - 1; i >= 0; i--){
+            reverse += input.charAt(i);
+        }
+        System.out.println("Reverse: " + reverse);
+
+
+    }
 }
