05fb7f70edc98ef3e751715a4afe91eda142cf1d
d0rarara <dorcaskumbu@outlook.com>
2024-11-17 11:24:12 -0500

    implement vehicleAvgSpeed class

---
+++ vehicleAvgSpeed.java
@@ -0,0 +1,14 @@
+public class vehicleAvgSpeed {
+    public static void main(String[] args)
+    {
+        double TIME;
+        double AVERAGE_SPEED;
+        System.out.println("This program calculates vehicle average speed" +
+        "\n given a time and distance traveled.");
+        TIME = 20.5;
+        AVERAGE_SPEED = distance / TIME;
+        System.out.println("Car average speed is " + AVERAGE_SPEED
+        + " miles per hour.");
+    }
+        public static final double distance = 180;
+}
