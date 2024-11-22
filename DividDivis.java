2434cd1c0144dbaa060b4b0b1833cc044c13df57
d0rarara <dorcaskumbu@outlook.com>
2024-11-22 17:45:47 -0500

    implement DividDivis class

---
+++ DividDivis.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class DividDivis {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int n2 = sc.nextInt();
+        int quotient = n1/n2;
+        int remainder = n1%n2;
+        System.out.println("Dividend: " + n1);
+        System.out.println("Divisor: " + n2);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+
+    }
+}
